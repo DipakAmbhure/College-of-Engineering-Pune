7c172d4e4d711d4ca3d441e73dd01badd02fe63f
Dipak Ambhure <61344089+DipakAmbhure@users.noreply.github.com>
2020-11-11 11:33:13 +0530

    Update fact.c

--- PPL/Assignments/Assignment3/fact.c
+++ PPL/Assignments/Assignment3/fact.c
@@ -28,4 +28,28 @@ Dump of assembler code for function main:
    0x00000000000011a8 <+42>:	pop    %rbp
    0x00000000000011a9 <+43>:	retq   
 End of assembler dump.
+
+
+Dump of assembler code for function fact:
+   0x0000000000001149 <+0>:	endbr64
+   0x000000000000114d <+4>:	push   %rbp
+   0x000000000000114e <+5>:	mov    %rsp,%rbp
+   0x0000000000001151 <+8>:	sub    $0x10,%rsp
+   0x0000000000001155 <+12>:	mov    %edi,-0x4(%rbp)
+   0x0000000000001158 <+15>:	cmpl   $0x1,-0x4(%rbp)
+   0x000000000000115c <+19>:	je     0x1164 <fact+27>
+   0x000000000000115e <+21>:	cmpl   $0x0,-0x4(%rbp)
+   0x0000000000001162 <+25>:	jne    0x116b <fact+34>
+   0x0000000000001164 <+27>:	mov    $0x1,%eax
+   0x0000000000001169 <+32>:	jmp    0x117c <fact+51>
+   0x000000000000116b <+34>:	mov    -0x4(%rbp),%eax
+   0x000000000000116e <+37>:	sub    $0x1,%eax
+   0x0000000000001171 <+40>:	mov    %eax,%edi
+   0x0000000000001173 <+42>:	callq  0x1149 <fact>
+   0x0000000000001178 <+47>:	imul   -0x4(%rbp),%eax
+   0x000000000000117c <+51>:	leaveq
+   0x000000000000117d <+52>:	retq
+End of assembler dump.
+
+
 */
